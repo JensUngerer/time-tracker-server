ae59579bca5d11f8164be1a76fbf140515d805f1
Jens Ungerer <github@jens-ungerer.de>
2019-07-19 05:03:25 +0200

    adding graceful shutdown logic with tracing via console.error

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,26 @@ const port: number = 3000;
 app.configure();
 app.configureExpress();
 app.listen(port);
+
+const gracefulShutdown: (shutdownMsg: string) => void = (shutdownMsg: string) => {
+    const shutdownPromise: Promise<boolean> = app.shutdown();
+    shutdownPromise.then(() => {
+        console.error(shutdownMsg);
+        console.error('process.exit()');
+        process.exit();
+    });
+    shutdownPromise.catch((err: any) => {
+        console.error(err);
+        console.error('process.exit()');
+        process.exit();
+    });
+};
+
+// https://nodejs.org/api/process.html#process_signal_events
+process.on('SIGINT', () => {
+    gracefulShutdown('SIGINT: CTRL+ C -> graceful shutdown completed -> process.exit()');
+});
+
+process.on('SIGHUP', () => {
+    gracefulShutdown('SIGHUP: window is closed -> graceful shutdown completed -> process.exit()');
+});
