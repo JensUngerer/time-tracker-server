1a168dfbf66289950cacef94e44283d0e3bbebde
Jens Ungerer <github@jens-ungerer.de>
2019-07-20 04:25:53 +0200

    using the previously commmited class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { AppManager } from './appManager';
 import app from './app';
 
 const port: number = 3000;
@@ -5,25 +6,4 @@ app.configure();
 app.configureExpress();
 app.listen(port);
 
-const gracefulShutdown: (shutdownMsg: string) => void = (shutdownMsg: string) => {
-    const shutdownPromise: Promise<boolean> = app.shutdown();
-    shutdownPromise.then(() => {
-        console.error(shutdownMsg);
-        console.error('process.exit()');
-        process.exit();
-    });
-    shutdownPromise.catch((err: any) => {
-        console.error(err);
-        console.error('process.exit()');
-        process.exit();
-    });
-};
-
-// https://nodejs.org/api/process.html#process_signal_events
-process.on('SIGINT', () => {
-    gracefulShutdown('SIGINT: CTRL+ C -> graceful shutdown completed -> process.exit()');
-});
-
-process.on('SIGHUP', () => {
-    gracefulShutdown('SIGHUP: window is closed -> graceful shutdown completed -> process.exit()');
-});
+AppManager.registerAppClosingEvent(app);
